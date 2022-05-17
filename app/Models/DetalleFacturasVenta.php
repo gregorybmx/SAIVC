35180154e138359e347ffd84bd2f73253cc44b60
Greivin Montoya Estrada <greivin.montoya.estrada@est.una.ac.cr>
2022-05-16 20:54:14 -0600

    Update DetalleFacturasVenta.php

--- app/Models/DetalleFacturasVenta.php
+++ app/Models/DetalleFacturasVenta.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class DetalleFacturasVenta extends Model
 {
     use HasFactory;
+    protected $table = 'detalle_factura_ventas';
+    protected $fillable = ['factura', 'producto', 'cantidad', 'precio_unitario', 'precio_total'];
+
+    public  function facturaVenta()
+    {
+        return $this -> belongsTo('App\Models\FacturaVenta', 'numeroFactura');
+    }
+
+    public  function producto()
+    {
+        return $this -> belongsTo('App\Models\Producto', 'codigo');
+    }
 }
