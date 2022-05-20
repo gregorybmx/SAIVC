4048fc04c0d21d157cb288d8f39e3024ba19bf75
Greivin Montoya Estrada <greivin.montoya.estrada@est.una.ac.cr>
2022-05-19 23:07:26 -0600

    Update DetalleFacturaVentaController.php
    Correcciones en pequeños errores del controller

--- app/Http/Controllers/DetalleFacturaVentaController.php
+++ app/Http/Controllers/DetalleFacturaVentaController.php
@@ -57,45 +57,49 @@ class DetalleFacturaVentaController extends Controller
     //store ->agrega o guarda elemento mediante metodo Post
     public  function store(Request $request)//Implementar comprobacion de existencia de archivo json
     {
+        $response = array(
+            'status' => 'error',
+            'code' => 406,
+            'message' => 'No se ha enviado el archivo con la informacion necesaria'
+        );
+
         $json = $request -> input('json',null);
-        $data = json_decode($json, true);
-        $data = array_map('trim', $data);
-
-        $rules = [
-            'factura' => 'required',
-            'producto' => 'required',
-            'cantidad' => 'required',
-            'precio_unitario' => 'required',
-            'precio_total' => 'required'
-        ];
-
-        $validate = \validator($data, $rules);
-
-        if ($validate->fails())
+        if($json)
         {
-            $response = array(
-                'status' => 'error',
-                'code' => 406,
-                'message' => 'Datos enviados no cumplen con las reglas establecidas ',
-                'errors' => $validate->errors()
-            );
-        }
+            $data = json_decode($json, true);
+            $data = array_map('trim', $data);
 
-        else
-        {
-            $detalleFactura = new DetalleFacturasVenta();
-            $detalleFactura -> factura = $data['factura'];
-            $detalleFactura -> producto = $data['producto'];
-            $detalleFactura -> cantidad = $data['cantidad'];
-            $detalleFactura -> precio_unitario = $data['precio_unitario'];
-            $detalleFactura -> precio_total = $data['precio_total'];
-            $detalleFactura -> save();
+            $rules = [
+                'factura' => 'required',
+                'producto' => 'required',
+                'cantidad' => 'required',
+                'precio_unitario' => 'required',
+                'precio_total' => 'required'
+            ];
 
-            $response = array(
-                'status' => 'success',
-                'code' => 200,
-                'message' => 'Detalle de Factura de Venta almacenado satisfactoriamente'
-            );
+            $validate = \validator($data, $rules);
+
+            if ($validate->fails())
+            {
+                $response['message'] = 'Datos enviados no cumplen con las reglas establecidas ';
+                $response['errors'] = $validate->errors();
+            }
+
+            else
+            {
+                $detalleFactura = new DetalleFacturasVenta();
+                $detalleFactura->factura = $data['factura'];
+                $detalleFactura->producto = $data['producto'];
+                $detalleFactura->cantidad = $data['cantidad'];
+                $detalleFactura->precio_unitario = $data['precio_unitario'];
+                $detalleFactura->precio_total = $data['precio_total'];
+                $detalleFactura->save();
+
+
+                $response['status'] = 'success';
+                $response['code'] = 201;
+                $response['message'] = 'Detalle de Factura de Venta almacenado satisfactoriamente';
+            }
         }
 
         return response()->json($response, $response['code']);
@@ -104,55 +108,56 @@ class DetalleFacturaVentaController extends Controller
     //update modifica elemento mediante Metodo Put
     public function update(Request $request)
     {
+        $response = array(
+            'status' => 'error',
+            'code' => 406,
+            'message' => 'No se ha enviado el archivo con la informacion necesaria'
+        );
+
         $json = $request -> input('json',null);
-        $data = json_decode($json, true);
-        $data = array_map('trim', $data);
 
-
-        $rules = [
-            'id' => 'required',
-            'producto' => 'required',
-            'cantidad' => 'required',
-            'precio_unitario' => 'required',
-            'precio_total' => 'required'
-        ];
-
-        $validate = \validator($data, $rules);
-
-        if($validate -> fails())
+        if($json)
         {
-            $response = array(
-                'status' => 'error',
-                'code' => 406,
-                'message' => 'Los datos enviados son incorrectos',
-                'errors' => $validate->errors()
-            );
-        }
+            $data = json_decode($json, true);
+            $data = array_map('trim', $data);
 
-        else
-        {
-            $id = $data['id'];
-            unset($data['id']);
-            unset($data['factura']);
 
-            $updated = DetalleFacturasVenta::where('id', $id) -> update($data);
+            $rules = [
+                'id' => 'required',
+                'producto' => 'required',
+                'cantidad' => 'required',
+                'precio_unitario' => 'required',
+                'precio_total' => 'required'
+            ];
 
-            if($updated > 0)
+            $validate = \validator($data, $rules);
+
+            if ($validate->fails())
             {
-                $response = array(
-                    'status' => 'success',
-                    'code' => 200,
-                    'message' => 'Datos actualizados exitosamente'
-                );
+                $response['message'] = 'Los datos enviados son incorrectos';
+                $response['errors'] = $validate->errors();
             }
 
             else
             {
-                $response = array(
-                    'status' => 'error',
-                    'code' => 400,
-                    'message' => 'No se pudo actualizar los datos'
-                );
+                $id = $data['id'];
+                unset($data['id']);
+                unset($data['factura']);
+
+                $updated = DetalleFacturasVenta::where('id', $id)->update($data);
+
+                if ($updated > 0)
+                {
+                    $response['status'] = 'success';
+                    $response['code'] = 200;
+                    $response['message'] = 'Datos actualizados exitosamente';
+                }
+
+                else
+                {
+                    $response['code'] = 400;
+                    $response['message'] = 'No se pudo actualizar los datos';
+                }
             }
         }
 
@@ -163,33 +168,25 @@ class DetalleFacturaVentaController extends Controller
     {
         if(isset($id))
         {
-            $deleted = DetalleFacturasVenta::where('id', $id) -> delete();
-
-            if($deleted)
-            {
-                $response=array(
-                    'status'=>'success',
-                    'code'=>200,
-                    'message'=>'Detalle Factura de Venta eliminada correctamente'
-                );
-            }
-
-            else{
-                $response=array(
-                    'status'=>'error',
-                    'code'=>400,
-                    'message'=>'No se pudo eliminar el Detalle de Factura, puede que el registro no exista'
-                );
-            }
-        }
-
-        else
-        {
             $response=array(
                 'status'=>'error',
                 'code'=>404,
                 'message'=>'Falta el identificador del Detalle de Factura'
             );
+
+            $deleted = DetalleFacturasVenta::where('id', $id) -> delete();
+
+            if($deleted)
+            {
+                $response['status'] = 'success';
+                $response['code'] =200;
+                $response['message'] = 'Detalle Factura de Venta eliminada correctamente';
+            }
+
+            else{
+                $response['code'] = 400;
+                $response['message'] = 'No se pudo eliminar el Detalle de Factura, puede que el registro no exista';
+            }
         }
 
         return response()->json($response, $response['code']);
