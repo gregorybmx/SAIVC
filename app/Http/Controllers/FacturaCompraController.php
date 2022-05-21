ee58340ecbcf861f0936ae77af9dd2ac0c1eb1c9
Greivin Montoya Estrada <greivin.montoya.estrada@est.una.ac.cr>
2022-05-20 18:02:57 -0600

    Update FacturaCompraController.php
    Corrección de error al no enviar archivo, (solo agregue un If XD)
    
    Se simplificaron los array de respuesta optimizando el código

--- app/Http/Controllers/FacturaCompraController.php
+++ app/Http/Controllers/FacturaCompraController.php
@@ -7,7 +7,7 @@ use App\Models\FacturaCompra;
 
 class FacturaCompraController extends Controller
 {
-    
+
     public function _construct(){
 
     }
@@ -18,7 +18,7 @@ class FacturaCompraController extends Controller
             'code' => '404',
             'data'=>'No se han agregado registros'
         );
-         $data=FacturaCompra::all();
+        $data=FacturaCompra::all();
 
         if(sizeof($data)>0){
             $response['status']= 'success';
@@ -29,156 +29,181 @@ class FacturaCompraController extends Controller
 
     }
     // devuelve un elemento por su id mediante GET
-    public function show($id){
+    public function show($id)
+    {
+        $response=array(
+            'status'=>'error',
+            'code'=>404,
+            'data'=>'Registro no encontrado'
+        );
+
         $data=FacturaCompra::find($id);
-        if(is_object($data)){
 
-            $response=array(
-                'status'=>'success',
-                'code'=>200,
-                'data'=>$data
-            );
-        }else{
-            $response=array(
-                'status'=>'error',
-                'code'=>404,
-                'message'=>'Registro no encontrado'
-
-            );
+        if(is_object($data))
+        {
+            $response ['status'] = 'success';
+            $response ['code'] = 200;
+            $response ['data'] = $data;
         }
+
         return response()->json($response,$response['code']);
-
     }
- 
-    // agrega un elemento mediante POST
-    public function store(Request $request){
-        $json = $request->input('json',null);
-        $data = json_decode($json,true);
-        if(!empty($data)){
-            $data = array_map('trim',$data);
-            $rules = [
-                'id' => 'required',
-                'proveedor' => 'required',
-                'fecha_compra' => 'required',
-                'fecha_vencimiento' => 'required',         
-                'monto_total'=>'required',
-            ];
-            $validate=\validator($data,$rules);
-            if($validate->fails()){
-                $response = array(
-                    'status' => 'error',
-                    'code' =>406,
-                    'message' =>'Error al eviar los datos',
-                    'errors' =>$validate->errors()
 
-                );
-            }else{
-                $compra = new FacturaCompra();
-                $compra -> id = $data['id'];
-                $compra -> proveedor = $data['proveedor'];
-                $compra -> fecha_compra = $data['fecha_compra'];
-                $compra -> fecha_vencimiento = $data['fecha_vencimiento'];
-                $compra -> monto_total = $data['monto_total'];
-                $compra -> save();
-                $response = array(
-                    'status' => 'success',
-                    'code' => 200,
-                    'message' => 'Datos almacenados correctamente'
-                );
+    // agrega un elemento mediante POST
+    public function store(Request $request)
+    {
+        $response = array(
+            'status' => 'error',
+            'code' => 406,
+            'message' => 'No se ha enviado el archivo con la informacion necesaria'
+        );
+
+        $json = $request->input('json',null);
+
+        if($json)
+        {
+            $data = json_decode($json, true);
+
+            if (!empty($data))
+            {
+                $data = array_map('trim', $data);
+
+                $rules = [
+                    'id' => 'required',
+                    'proveedor' => 'required',
+                    'fecha_compra' => 'required',
+                    'fecha_vencimiento' => 'required',
+                    'monto_total' => 'required',
+                ];
+
+                $validate = \validator($data, $rules);
+
+                if ($validate->fails()) {
+                    $response ['message'] = 'Error al eviar los datos';
+                    $response ['errors'] = $validate->errors();
+                }
+
+                else
+                {
+                    $compra = new FacturaCompra();
+                    $compra->id = $data['id'];
+                    $compra->proveedor = $data['proveedor'];
+                    $compra->fecha_compra = $data['fecha_compra'];
+                    $compra->fecha_vencimiento = $data['fecha_vencimiento'];
+                    $compra->monto_total = $data['monto_total'];
+                    $compra->save();
+                    $response ['status'] = 'success';
+                    $response ['code'] = 200;
+                    $response ['message'] = 'Datos almacenados correctamente';
+                }
             }
-        }else{
-            $response = array(
-                'status'=>'error',
-                'code' => 400,
-                'message' => 'Faltan datos'
-            );
+
+            else
+            {
+                $response ['code'] = 400;
+                $response ['message'] = 'Faltan datos';
+            }
         }
-        return response()->json($response,$response['code']); 
+
+        return response()->json($response,$response['code']);
     }
 
     //modifica un elemento mediante PUT
-    public function update(Request $request){
-        $json = $request->input('json',null);
-        $data = json_decode($json,true);
-        if(!empty($data)){
-            $data =array_map('trim',$data);
-            $rules = [
-                'proveedor' => 'required',
-                'fecha_compra' => 'required',
-                'fecha_vencimiento'=> 'required',
-                'monto_total' => 'required',
-            ];
-            $validate=\validator($data,$rules);
-            if($validate -> fails()){
-                $response = array(
-                    'status' => 'error',
-                    'code' => 406,
-                    'message' => 'Error al eviar los datos',
-                    'errors' =>$validate->errors()
-                );
+    public function update(Request $request)
+    {
+        $response = array(
+            'status' => 'error',
+            'code' => 406,
+            'message' => 'No se ha enviado el archivo con la informacion necesaria'
+        );
 
-            }else{
-                $id = $data['id'];
-                unset($data['id']);
-                unset($data['created_at']);
-                unset($data['updated_at']);
-                $updated = FacturaCompra::where('id',$id)->update($data);
-                if($updated > 0){
-                    $response = array(
-                        'status' => 'success',
-                        'code' => 200,
-                        'message' => 'Datos actualizados correctamente'
-                    );
-                }else{
-                    $response = array( 
-                        'status' => 'error',
-                        'code' => 404,
-                        'message' => 'Error al actualizar los datos'
-                    );
+        $json = $request->input('json',null);
+
+        if($json)
+        {
+            $data = json_decode($json, true);
+
+            var_dump($data);
+
+            if (!empty($data))
+            {
+                $data = array_map('trim', $data);
+
+                $rules = [
+                    'proveedor' => 'required',
+                    'fecha_compra' => 'required',
+                    'fecha_vencimiento' => 'required',
+                    'monto_total' => 'required',
+                ];
+
+                $validate = \validator($data, $rules);
+
+                if ($validate->fails())
+                {
+                    $response ['message'] = 'Error al eviar los datos';
+                    $response ['errors'] = $validate -> errors();
                 }
 
+                else
+                {
+                    $id = $data['id'];
+                    unset($data['id']);
+                    unset($data['created_at']);
+                    unset($data['updated_at']);
+
+                    $updated = FacturaCompra::where('id', $id)->update($data);
+
+                    if ($updated > 0)
+                    {
+                        $response ['status'] = 'success';
+                        $response ['code'] = 200;
+                        $response ['message'] = 'Datos actualizados correctamente';
+                    }
+
+                    else
+                    {
+                        $response ['code'] = 404;
+                        $response ['message'] = 'Error al actualizar los datos';
+                    }
+                }
             }
-        }else{
-            $response = array(
-                'status' => 'error',
-                'code' => 400,
-                'message' => 'Faltan Datos'
-            );
+
+            else
+            {
+                $response ['code'] = 400;
+                $response ['message'] = 'Faltan Datos';
+            }
         }
+
         return response()->json($response,$response['code']);
     }
 
 
     //elimina un elemento mediante DELETE
-    public function destroy($id){
-        if(isset($id)){
+    public function destroy($id)
+    {
+        $response = array(
+            'status' => 'error',
+            'code' => 401,
+            'message' => 'Faltan elementos'
+        );
+
+        if(isset($id))
+        {
             $deleted = FacturaCompra::where('id',$id)->delete();
             if($deleted){
-                $response = array(
-                    'status' => 'succes',
-                    'code' =>200,
-                    'message' => 'Elemento eliminado correctamente'
-
-                );
-               
-            }else{
-                $response = array(
-                    'status' => 'error',
-                    'code' => 400,
-                    'message' => 'Error al eliminar el elemento'
-
-                );
+                $response ['status'] = 'succes';
+                $response ['code'] = 200;
+                $response ['message'] = 'Elemento eliminado correctamente';
             }
-        }else{
-            $response = array(
-                'status' => 'error',
-                'code' => 401,
-                'message' => 'Faltan elementos'
-            );
+
+            else
+            {
+                $response ['code'] = 400;
+                $response ['message'] = 'Error al eliminar el elemento';
+            }
         }
+
         return response()->json($response,$response['code']);
-
     }
-
-
 }
