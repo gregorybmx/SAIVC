2d3684e4c720a5e762add4069af9141cc9c483f0
Greivin Montoya Estrada <greivin.montoya.estrada@est.una.ac.cr>
2022-05-26 14:37:38 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\DetalleFacturaVentaController;
 use App\Http\Controllers\FacturaCompraController;
 use App\Http\Controllers\ProductoController;
 use App\Http\Controllers\ProveedorController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,7 +20,13 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::prefix('api')->group(function(){
+Route::prefix('api')->group(function()
+{
+    Route::post('/user/upload',[UserController::class,'uploadImage']);
+    Route::get('user/getimage/{filename}',[UserController::class, 'getImage']);
+    Route::post('/user/login',[UserController::class, 'login']);
+    Route::get('/user/getidentity',[UserController::class, 'getIdentity']);
+    
     //Rutas Automaticas RESTful
     Route::resource('/facturaventa', FacturaVentaController::class,['except'=>['create','edit']]);
     Route::resource('/detallefacturaventa', DetalleFacturaVentaController::class,['except'=>['create','edit']]);
@@ -28,4 +35,5 @@ Route::prefix('api')->group(function(){
     Route::resource('/facturacompra', FacturaCompraController::class,['except'=>['create','edit']]);
     Route::resource('/producto', ProductoController::class,['except'=>['create','edit']]);
     Route::resource('/proveedor', ProveedorController::class,['except'=>['create','edit']]);
+    Route::resource('/user', UserController::class,['except'=>['create','edit','login', 'getIdentity']]);
 });
