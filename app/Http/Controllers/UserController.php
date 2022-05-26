70d31adc3e660f5ef3f21bfd1724b328eb98725e
Greivin Montoya Estrada <greivin.montoya.estrada@est.una.ac.cr>
2022-05-26 14:37:35 -0600

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,8 @@ use App\Helpers\JwtAuth;
 
 class UserController extends Controller
 {
-    public function __construct(){
+    public function __construct()
+    {
         $this->middleware('api.auth',['except'=>['show','login','store','getImage']]);
     }
     public function __invoke(){
@@ -31,148 +32,222 @@ class UserController extends Controller
         return response()->json($response, $response['code']);
     }
 
-    public function store(Request $request){
+    public function show($id)
+    {
+        $response = array(
+            'status' => 'error',
+            'code' => 404,
+            'data' => 'Recurso no encontrado'
+        );
+
+        if(isset($id))
+        {
+            $user = User::find($id);
+
+            if(is_object($user))
+            {
+                $response['status'] = 'success';
+                $response['code'] = 200;
+                $response['data'] = $user;
+            }
+        }
+
+        else
+        {
+            $response['code'] = 409;
+            $response['data'] = 'No se ha ingresado el Id deseado';
+        }
+
+        return response() -> json($response, $response['code']);
+    }
+
+    public function store(Request $request)
+    {
+        $response = array(
+            'status' => 'error',
+            'code' => 406,
+            'message' => 'No se ha enviado el archivo con la informacion necesaria'
+        );
 
         $json = $request->input('json', null);
-        $data = json_decode($json, true);
-        $data = array_map('trim', $data);
-        $rules = [
-            'id' => 'required',
-            'name' => 'required|alpha',
-            'last_name' => 'required',
-            'role' => 'required',
-            'email' => 'required|email|unique:users',
-            'password' => 'required',
-        ];
-        $validate = \validator($data,$rules);
-        if($validate->fails()) {
-            $response = array(
-                'status' => 'error',
-                'code' => 404,
-                'menssage' => 'El usuario no se ha creado',
-                'errors' => $validate->errors(),
-            );
-        } 
         
-        else{
-            $user = new User();
-            $user->id= $data['id'];
-            $user->name= $data['name'];
-            $user->last_name= $data['last_name'];
-            $user->email= $data['email'];
-            $user->password= hash('sha256',$data['password']);;
-            $user->role = $data['role'];
-            $user->save();
-            $response = array(
-                'status' => 'success',
-                'code' => 200,
-                'menssage' => 'El usuario se ha creado',
-                'username' => $user
-            );
+        if($json)
+        {
+            $data = json_decode($json, true);
+            $data = array_map('trim', $data);
+            $rules = [
+                'id' => 'required',
+                'name' => 'required|alpha',
+                'last_name' => 'required',
+                'role' => 'required',
+                'email' => 'required|email|unique:users',
+                'password' => 'required',
+            ];
+            $validate = \validator($data,$rules);
+            
+            if($validate->fails()) 
+            {
+                $response['code'] = 404;
+                $response['menssage'] = 'El usuario no se ha creado';
+                $response['errors'] = $validate->errors();
+            } 
+        
+            else
+            {
+                $user = new User();
+                $user->id= $data['id'];
+                $user->name= $data['name'];
+                $user->last_name= $data['last_name'];
+                $user->email= $data['email'];
+                $user->password= hash('sha256',$data['password']);;
+                $user->role = $data['role'];
+                $user->save();
+                
+                $response['status'] = 'success';
+                $response['code'] = 200;
+                $response['menssage'] = 'El usuario se ha creado';
+                $response['username'] = $user;
+            }
         }
+
         return response()->json($response, $response['code']);
     }
 
-    public function update(Request $request){
+    public function update(Request $request)
+    {
+        $response = array(
+            'status' => 'error',
+            'code' => 406,
+            'message' => 'No se ha enviado el archivo con la informacion necesaria'
+        );
+
         $json=$request->input('json',null);
-        $data=json_decode($json,true);
-        $data=array_map('trim',$data);
-        $rules = [
-            'id' => 'required|alpha',
-            'name' => 'required|alpha',
-            'last_name' => 'required|alpha',
-            'role' => 'required|alpha',
-            'email' => 'required|email|unique:users',
-            'password' => 'required|alpha',
-        ];
-        $valid=\validator($data,$rules);
-        if($valid->fails()){
-            $response=array(
-                'status' => 'error',
-                'code' => 406,
-                'message' => 'Datos incorrectos',
-                'errors'=>$valid->errors()
-            );
-        } else{
-            $email=$data['email'];
-            unset($data['id']);
-            unset($data['created_at']);
-            unset($data['remember_token']);
-            $updated=User::where('email',$email)->update($data);
-            if($updated>0){
-                $response = array(
-                    'status' => 'success',
-                    'code' => 200,
-                    'message' => 'Datos actualizados'
-                );
-            } else{
-                $response=array(
-                    'status' =>'error',
-                    'code' => 400,
-                    'message' => 'Error al actualizar los datos'
-                );
+
+        if($json)
+        {
+            $data=json_decode($json,true);
+            $data=array_map('trim',$data);
+            
+            $rules = [
+                'id' => 'required',
+                'name' => 'required|alpha',
+                'last_name' => 'required|alpha',
+                'role' => 'required',
+                'email' => 'required|email',
+                'password' => 'required',
+            ];
+        
+            $valid=\validator($data,$rules);
+            
+            if($valid->fails())
+            {
+                $response['message'] = 'Datos incorrectos';
+                $response['errors'] = $valid->errors();
+            } 
+            
+            else
+            {
+                $email = $data['email'];
+
+                unset($data['id']);
+                unset($data['created_at']);
+                unset($data['remember_token']);
+
+                $data['password']= hash('sha256',$data['password']);
+
+                $updated=User::where('email',$email)->update($data);
+                
+                if($updated>0)
+                {
+                    $response['status'] = 'success';
+                    $response['code'] = 200;
+                    $response['message'] = 'Datos actualizados';
+                } 
+
+                else
+                {
+                    $response['code'] = 400;
+                    $response['message'] = 'Error al actualizar los datos';
+                }
             }
         }
+        
         return response()->json($response,$response['code']);
     }
 
-    public function destroy($id){
+    public function destroy($id)
+    {
+        $response=array(
+            'status'=>'error',
+            'code'=>400,
+            'message'=>'Falta el identificador'
+        );
+
         if(isset($id)){
+            
             $deleted=User::where('id',$id)->delete();
-            if($deleted){
-                $response=array(
-                    'status'=>'success',
-                    'code'=>200,
-                    'message'=>'Usuario eliminado'
-                );
-            }else{
-                $response=array(
-                    'status'=>'error',
-                    'code'=>400,
-                    'message'=>'No se pudo eliminar'
-                );
+            
+            if($deleted)
+            {
+                $response['status'] = 'success';
+                $response['code'] = 200;
+                $response['message'] = 'Usuario eliminado';
             }
-        }else{
-            $response=array(
-                'status'=>'error',
-                'code'=>400,
-                'message'=>'Falta el identificador'
-            );
+            
+            else
+            {
+                $response['message'] = 'No se pudo eliminar';
+            }
         }
+
         return response()->json($response,$response['code']);
     }
 
-    public function uploadImage(Request $request){
+    public function uploadImage(Request $request)
+    {
+        $response=array(
+            'status'=>'success',
+            'code'=>200,
+            'message'=>'Imagen guardada correctamente',
+            'image_name'=>$filename
+        );
+
         $image=$request->file('file0');
+
         $valid= \Validator::make($request->all(),[
             'file0'=>'required|image|mimes:jpg,png'
         ]);
-        if(!$image||$valid->fails()){
-            $response=array(
-                'status'=>'error',
-                'code'=>406,
-                'message'=>'Error al subir el archivo',
-                'errors'=>$valid->errors()
-            );
-        }else{
-            $filename=time().$image->getClientOriginalName();
-            \Storage::disk('users')->put($filename,\File::get($image));
-            $response=array(
-                'status'=>'success',
-                'code'=>200,
-                'message'=>'Imagen guardada correctamente',
-                'image_name'=>$filename
-            );
+
+        if(!$image||$valid->fails())
+        {
+            $response['status'] = 'error';
+            $response['code'] = 406;
+            $response['message'] = 'Error al subir el archivo';
+            $response['errors'] = $valid->errors();
         }
+        
+        else
+        {
+            $filename=time().$image->getClientOriginalName();
+
+            \Storage::disk('users')->put($filename,\File::get($image));
+        }
+
         return response()->json($response,$response['code']);
     }
 
-    public function getImage($filename){
+    public function getImage($filename)
+    {    
         $exist=\Storage::disk('users')->exists($filename);
-        if($exist){
+        
+        if($exist)
+        {
             $file=\Storage::disk('users')->get($filename);
             return new Response($file,200);
-        }else{
+        }
+        
+        else
+        {
             $response=array(
                 'status'=>'error',
                 'code'=>404,
