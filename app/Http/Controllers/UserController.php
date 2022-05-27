2dc7b1ca61caca125d5a37c947bae687ba75b6c7
Greivin Montoya Estrada <greivin.montoya.estrada@est.una.ac.cr>
2022-05-26 20:14:39 -0600

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,6 +13,7 @@ class UserController extends Controller
     {
         $this->middleware('api.auth',['except'=>['show','login','store','getImage']]);
     }
+    
     public function __invoke(){
 
     }
